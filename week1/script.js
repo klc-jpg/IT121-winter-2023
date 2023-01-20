669d6d41c86282b7c3e443e78221994d0d74ec8a
klc-jpg <72785327+klc-jpg@users.noreply.github.com>
2023-01-19 19:39:23 -0600

    Week one commit

---
+++ week1/script.js
@@ -0,0 +1,6 @@
+var firstName = "Kelly";
+var lastName = "Chapman";
+var yearsOfStudy = 2;
+var goal = "developer";
+
+document.write(firstName + ' ' + lastName + '.' + ' I have been at SCC for ' + yearsOfStudy + ' years, and I plan to work as a ' + goal + ' after I complete my courses.');
